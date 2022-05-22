5da5d465b86fc8798c62f77bbb192e3487eb5baf
Antonio J. Delgado <ad@susurrando.com>
2022-05-22 23:29:35 +0300

    fix configuration

--- install.sh
+++ install.sh
@@ -18,33 +18,34 @@ if [ ! -e /etc/rc_push.conf ]; then
 
     echo "Rocket.Chat user name:"
     read -r rc_user
-    echo "user='${rc_user}" >> /etc/rc_push.conf
+    echo "user='${rc_user}'" >> /etc/rc_push.conf
 
     echo "Rocket.Chat user password:"
     read -s -r rc_pass
-    echo "password='${rc_pass}" >> /etc/rc_push.conf
+    echo "password='${rc_pass}'" >> /etc/rc_push.conf
 
     echo "URL to your ntfy instance:"
     read -r ntfy_url
-    echo "ntfy_url='${ntfy_url}" >> /etc/rc_push.conf
+    echo "ntfy_url='${ntfy_url}'" >> /etc/rc_push.conf
 
     echo "Topic in ntfy:"
     read -r ntfy_topic
-    echo "ntfy_topic='${ntfy_topic}" >> /etc/rc_push.conf
+    echo "ntfy_topic='${ntfy_topic}'" >> /etc/rc_push.conf
 
     echo "User name in ntfy:"
     read -r ntfy_user
-    echo "ntfy_user='${ntfy_user}" >> /etc/rc_push.conf
+    echo "ntfy_user='${ntfy_user}'" >> /etc/rc_push.conf
 
     echo "User password in ntfy:"
     read -s -r ntfy_pass
-    echo "ntfy_pass='${ntfy_pass}" >> /etc/rc_push.conf
+    echo "ntfy_pass='${ntfy_pass}'" >> /etc/rc_push.conf
 
     echo "If you only want to monitor certain channels, enter them here separated by commas:"
     read -r channels
-    echo "channels=['${channels//,/','}']" >> /etc/rc_push.conf
+    echo "channels=['${channels//,/\',\'}']" >> /etc/rc_push.conf
 fi
 
+chown rc_push /etc/rc_push.conf
 chmod go-rwx /etc/rc_push.conf
 
 cat << EOF > /etc/systemd/system/rc_push.service
