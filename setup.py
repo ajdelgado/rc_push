fe18ecbdf35f7fe5f07f30dbaa3b6a60e5d79f30
Antonio J. Delgado <ad@susurrando.com>
2022-05-22 22:54:36 +0300

    Fix setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setuptools.setup(
     scripts=['rc_push/rc_push.py'],
     author="Antonio J. Delgado",
     version='0.0.1',
-    name='__project_codenane__',
+    name='rc_push',
     author_email="",
     url="",
     description="__description__",
